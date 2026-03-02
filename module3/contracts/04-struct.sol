371331a5e77421a06ed6499bdfd6ae73764ca9a8
Palash A <palash.agarwal.96@gmail.com>
2026-03-01 21:46:40 -0500

    finish 04

--- module3/contracts/04-struct.sol
+++ module3/contracts/04-struct.sol
@@ -1,17 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 contract Cars {
 
     enum CarStatus { driving, parked }
 
-    ___ Car {
-        ___ colour;
-        ___ doors;
-        ___ status;
-        ___ owner;
+    struct Car {
+        bytes3 colour;
+        uint8 doors;
+        CarStatus status;
+        address owner;
     }
 
+    // making the struct public will make all of its fields public
+    Car public car;
+
     constructor() {
+        car = Car(0xff0000, 4, CarStatus.parked, msg.sender);
     }
 
 }
